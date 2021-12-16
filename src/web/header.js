c0ad4d6106f1aa00edfd7b4562e8bc926633f07b
zeeshan400 <tabahidavil@gmail.com>
2021-12-16 11:42:02 +0500

    initial commit

---
+++ src/web/header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './css/header.css'
+export default function Header() {
+    return (
+        <div className='header'>
+                      
+         <div className='header_left'>
+        </div> 
+        <div className="header_right"></div>
+            </div>   
+    )
+}
